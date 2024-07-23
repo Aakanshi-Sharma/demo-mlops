fc8deda7aa5893f7757430900306be5a3bffda0b
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2024-07-23 10:51:53 +0530

    get data

---
+++ src/get_data.py
@@ -0,0 +1,22 @@
+import os
+import yaml
+import pandas as pd
+import argparse
+
+def get_data(config_path):
+    config=read_config(config_path)
+    data_path=config["data_source"]["s3_source"]
+    df=pd.read_csv(data_path, encoding="utf-8")
+    print(df.head())
+
+def read_config(config_path):
+    with open(config_path, 'r') as yaml_file:
+        config=yaml.safe_load(yaml_file)
+    return config
+
+
+if __name__ == '__main__':
+    args=argparse.ArgumentParser()
+    args.add_argument('--config', default='params.yaml')
+    parsed_args=args.parse_args()
+    get_data(parsed_args.config)
