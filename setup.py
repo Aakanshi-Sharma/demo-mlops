09a92ab4dbc3a010ae6775a845ddb09706265f78
Aakanshi <56083_aakanshisharma@gbpuat-tech.ac.in>
2024-07-28 12:42:10 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    description="It's mlops project",
+    version="0.0.1",
+    author="aakanshi",
+    packages=find_packages(),
+    license="MIT",
+)
